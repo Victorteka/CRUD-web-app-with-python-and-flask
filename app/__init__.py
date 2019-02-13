03ed2e8728fe157648fa95e274c6a420d45e5a4c
Victor Teka <ambukateka@gmail.com>
2019-02-13 18:10:11 +0300

    add blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -19,4 +19,13 @@ def create_app(config_name):
     loginmanager.login_message = 'You must be logged in to access this page'
     loginmanager.login_view = 'auth.login'
 
+    from .admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
+
     return app
