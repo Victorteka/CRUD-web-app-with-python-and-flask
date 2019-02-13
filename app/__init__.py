861b2b3897069e1d95300f56594274daa79a12db
Victor Teka <ambukateka@gmail.com>
2019-02-13 15:16:38 +0300

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+#third party imports
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+#local imports
+from config import app_config
+
+#db variable initialization
+db = SQLAlchemy()
+loginmanager = LoginManager()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(app_config[config_name])
+    db.init_app(app)
+    loginmanager.init_app(app)
+    loginmanager.login_message = 'You must be logged in to access this page'
+    loginmanager.login_view = 'auth.login'
+
+
+
+    return app
